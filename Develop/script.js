2179473627c534dffc72c032e9ca94208fc5247b
JasonKim0122 <JasonKim0122@gmail.com>
2022-08-04 21:02:33 -0500

    created variables for pw generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,16 @@
-// Assignment code here
+//Variables
+
+// Variables for upper and lower case letters
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+var numbers = '0123456789';
+var symbols = '!#$%&\()*+,-./:;<=>?@^[\\]^_`{|}~';
+
+//Variables to grab what is in between the ''
+var upperCaseChar = upperCase.split('');
+var lowerCaseChar = lowerCase.split('');
+var numbersChar = numbers.split('');
+var symbolsChar = symbols.split('');
 
 
 // Get references to the #generate element
